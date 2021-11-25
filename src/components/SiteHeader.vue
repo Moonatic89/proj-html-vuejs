7576481c446c2b9381651c6c97219932ffda233b
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-25 11:43:14 +0100

    TEST - Protorype header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,22 @@
 <template>
   <header>
+    <nav>
+      <div class="logo">
+        <img src="../assets/img/logo-sidearea-1.png" alt="" />
+      </div>
+      <div class="navbar">
+        <ul>
+          <li>HOME</li>
+          <li>PAGES</li>
+          <li>PORTFOLIO</li>
+          <li>BLOG</li>
+          <li>SHOP</li>
+          <li>ELEMENTS</li>
+          <li>img here</li>
+        </ul>
+      </div>
+    </nav>
+
     <h1>SITE HEADER</h1>
   </header>
 </template>
@@ -9,4 +26,19 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+header {
+  height: 90px;
+  background-color: wheat;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+
+    ul {
+      display: flex;
+
+      list-style: none;
+    }
+  }
+}
 </style>
