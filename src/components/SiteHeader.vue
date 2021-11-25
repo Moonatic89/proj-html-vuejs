eed74bc28bd5273445590f8e98ee7b23161fae61
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-25 12:19:12 +0100

    TEST - Working on Header Border

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -6,18 +6,36 @@
       </div>
       <div class="navbar">
         <ul>
-          <li>HOME</li>
-          <li>PAGES</li>
-          <li>PORTFOLIO</li>
-          <li>BLOG</li>
-          <li>SHOP</li>
-          <li>ELEMENTS</li>
-          <li>img here</li>
+          <li>
+            HOME
+            <div class="navOverlay"></div>
+          </li>
+          <li>
+            PAGES
+            <div class="navOverlay"></div>
+          </li>
+          <li>
+            PORTFOLIO
+            <div class="navOverlay"></div>
+          </li>
+          <li>
+            BLOG
+            <div class="navOverlay"></div>
+          </li>
+          <li>
+            SHOP
+            <div class="navOverlay"></div>
+          </li>
+          <li>
+            ELEMENTS
+            <div class="navOverlay"></div>
+          </li>
+          <li class="img">
+            <img src="../assets/img/svg-1.svg" alt="" />
+          </li>
         </ul>
       </div>
     </nav>
-
-    <h1>SITE HEADER</h1>
   </header>
 </template>
 
@@ -26,18 +44,46 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+@import "../assets/scss/colors.scss";
+
 header {
   height: 90px;
-  background-color: wheat;
 
   nav {
+    height: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
+
+    .logo {
+      padding: 1rem;
+
+      img {
+        height: 100%;
+      }
+    }
 
     ul {
       display: flex;
 
       list-style: none;
+
+      li img {
+        transform: translate(0, -12.5%);
+      }
+
+      li {
+        padding: 0.5rem 1.5rem;
+      }
+
+      .navOverlay {
+        position: relative;
+        z-index: -1;
+        transform: translate(-10%, -75%);
+        width: 120%;
+        height: 50%;
+        background-color: $headerBorder;
+      }
     }
   }
 }
