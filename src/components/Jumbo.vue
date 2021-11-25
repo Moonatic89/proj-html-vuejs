f4998d86584c942a03a7dd59f93ef915b2389dbd
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-25 16:40:55 +0100

    Working on Jumbo

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="Jumbo">
-    <h2>JUMBO</h2>
+    <div class="container">
+      <div class="card">
+        <div class="cardTitle">Devotion that never <span>ends</span></div>
+        <div class="cardSubtitle">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, quaerat?
+          Officia quaerat expedita voluptate aut? Enim sequi pariatur assumenda
+          in.
+        </div>
+        <div class="cardBtn">READ MORE</div>
+      </div>
+      <div class="jumboImage">
+        <div class="humans">
+          <div class="girl">
+            <img src="../assets/img/h-2-slider-img-15.png" alt="" />
+          </div>
+          <div class="boy">
+            <img src="../assets/img/h-2-slider-img-16.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,9 +29,55 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-#jumbo {
+#Jumbo {
   height: 644px;
   width: 100%;
-  background-color: chocolate;
+
+  .container {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .card {
+      width: 600px;
+      // align-self: center;
+      .cardTitle {
+        font-size: 5rem;
+        line-height: 70px;
+
+        span {
+          font-style: italic;
+          font-weight: bold;
+        }
+      }
+      .cardSubtitle {
+        padding: 2rem 0;
+        line-height: 25px;
+
+        color: gray;
+      }
+      .cardBtn {
+        font-size: 0.8rem;
+        background-color: white;
+        border: 2px solid red;
+        width: 200px;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+      }
+    }
+
+    .jumboImage {
+      width: 900px;
+      background-color: turquoise;
+      position: relative;
+
+      .humans {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
 }
 </style>
