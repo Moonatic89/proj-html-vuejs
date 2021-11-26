eeb30c69d01a34c4bbcc3ca62e678a8c3acde902
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-26 16:55:34 +0100

    TEST - Working on Workflow Component

--- src/components/Workflow.vue
+++ src/components/Workflow.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="Workflow">
-    <div class="WipText">
-      <h2>Working <span>process</span></h2>
+    <div class="wipText">
+      <div class="title">Working <span>process</span></div>
     </div>
 
-    <div class="art"></div>
+    <div class="art"><img src="" alt="" /></div>
 
     <div class="container">
       <div class="row">
-        <div class="col_3"><i class="fas fa-lightbulb"></i></div>
-        <div class="col_3"><i class="far fa-comment"></i></div>
-        <div class="col_3"><i class="fas fa-cloud"></i></div>
-        <div class="col_3"></div>
+        <div class="col_3">
+          <div class="iconBox">
+            <i class="fas fa-lightbulb greenish"></i>
+          </div>
+        </div>
+        <div class="col_3">
+          <div class="iconBox">
+            <i class="far fa-comment yellowish"></i>
+          </div>
+        </div>
+        <div class="col_3">
+          <div class="iconBox">
+            <i class="fas fa-cloud bordeaux"></i>
+          </div>
+        </div>
+        <div class="col_3">
+          <div class="iconBox">
+            <i class="fas fa-pencil-alt orangeish"></i>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,5 +37,57 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/colors.scss";
+
+#Workflow {
+  .wipText {
+    width: 100%;
+    text-align: center;
+    padding: 3rem;
+    .title {
+      font-size: 4rem;
+
+      span {
+        font-style: italic;
+        font-weight: bold;
+      }
+    }
+  }
+
+  .row {
+    display: flex;
+    margin: auto;
+    width: 55%;
+    justify-content: space-between;
+
+    .iconBox {
+      background-color: $workingBacgkround;
+      width: 162px;
+      height: 162px;
+      line-height: 162px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+
+      .greenish {
+        color: $iconGreen;
+      }
+      .yellowish {
+        color: $iconYellow;
+      }
+      .bordeaux {
+        color: $iconBordeaux;
+      }
+      .orangeish {
+        color: $iconOrange;
+      }
+
+      i {
+        font-size: 3.5rem;
+      }
+    }
+  }
+}
 </style>
