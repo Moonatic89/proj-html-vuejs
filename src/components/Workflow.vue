0f21d93b8c21a510025940ad8981e8ccd402943a
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-26 16:36:06 +0100

    TEST - Create other components, starting with Workflow

---
+++ src/components/Workflow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Workflow">
+    <div class="WipText">
+      <h2>Working <span>process</span></h2>
+    </div>
+
+    <div class="art"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col_3"><i class="fas fa-lightbulb"></i></div>
+        <div class="col_3"><i class="far fa-comment"></i></div>
+        <div class="col_3"><i class="fas fa-cloud"></i></div>
+        <div class="col_3"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
