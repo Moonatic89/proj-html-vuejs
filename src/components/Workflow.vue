352b8f470677fa00813db3232979aa5e72061e9b
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-28 14:35:57 +0100

    SUMMARY - Workflow
    Design finished, again font is missing

--- src/components/Workflow.vue
+++ src/components/Workflow.vue
@@ -14,22 +14,58 @@
       <div class="row">
         <div class="col_3">
           <div class="iconBox">
-            <i class="fas fa-lightbulb greenish"></i>
+            <div class="icon">
+              <i class="fas fa-lightbulb greenish"></i>
+            </div>
+
+            <div class="iconTitle">First theres an idea</div>
+
+            <div class="iconSubText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              natus ducimus adipisci? Vero, amet consequuntur!
+            </div>
           </div>
         </div>
         <div class="col_3">
           <div class="iconBox">
-            <i class="far fa-comment yellowish"></i>
+            <div class="icon">
+              <i class="far fa-comment yellowish"></i>
+            </div>
+
+            <div class="iconTitle">Then we talk about it</div>
+
+            <div class="iconSubText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              natus ducimus adipisci? Vero, amet consequuntur!
+            </div>
           </div>
         </div>
         <div class="col_3">
           <div class="iconBox">
-            <i class="fas fa-cloud bordeaux"></i>
+            <div class="icon">
+              <i class="fas fa-cloud bordeaux"></i>
+            </div>
+
+            <div class="iconTitle">And we think aout it</div>
+
+            <div class="iconSubText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              natus ducimus adipisci? Vero, amet consequuntur!
+            </div>
           </div>
         </div>
         <div class="col_3">
           <div class="iconBox">
-            <i class="fas fa-pencil-alt orangeish"></i>
+            <div class="icon">
+              <i class="fas fa-pencil-alt orangeish"></i>
+            </div>
+
+            <div class="iconTitle">So we draw along</div>
+
+            <div class="iconSubText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              natus ducimus adipisci? Vero, amet consequuntur!
+            </div>
           </div>
         </div>
       </div>
@@ -75,34 +111,58 @@ export default {};
   .row {
     display: flex;
     margin: auto;
-    width: 55%;
+    width: 60%;
     justify-content: space-between;
 
-    .iconBox {
-      background-color: $workingBacgkround;
-      width: 162px;
-      height: 162px;
-      line-height: 162px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 50%;
+    .col_3 {
+      width: calc(100% / 4);
 
-      .greenish {
-        color: $iconGreen;
-      }
-      .yellowish {
-        color: $iconYellow;
-      }
-      .bordeaux {
-        color: $iconBordeaux;
-      }
-      .orangeish {
-        color: $iconOrange;
-      }
+      .iconBox {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
 
-      i {
-        font-size: 3.5rem;
+        .icon > i {
+          vertical-align: middle;
+          font-size: 3.5rem;
+        }
+
+        .icon {
+          background-color: $workingBacgkround;
+          width: 162px;
+          height: 162px;
+          line-height: 162px;
+          border-radius: 50%;
+
+          .greenish {
+            color: $iconGreen;
+          }
+          .yellowish {
+            color: $iconYellow;
+          }
+          .bordeaux {
+            color: $iconBordeaux;
+          }
+          .orangeish {
+            color: $iconOrange;
+          }
+        }
+
+        .iconText {
+          width: 50%;
+        }
+        .iconTitle {
+          margin: 1rem 0;
+          font-size: 1.5rem;
+          font-weight: bold;
+        }
+        .iconSubText {
+          width: 80%;
+          color: gray;
+          font-size: 0.8rem;
+        }
       }
     }
   }
